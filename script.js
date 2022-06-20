d89dd4abea29a61703de4dc1d3daeebd0f031c7f
hasemon <abdulhaseebshoukat24@outlook.con>
2022-06-20 13:54:48 +0500

    Add functionality for changing container dimensions

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 container = document.querySelector(".container")
+createGrid(16);
 
-for (let i = 0; i < 16; i++) {
-    row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-        rowItem = document.createElement("div");
-        rowItem.classList.add("row-item");
-        row.appendChild(rowItem)
-    }
-    container.appendChild(row)
-}
-
-rowItems = document.querySelectorAll(".row-item");
-rowItems.forEach(item => {
-    item.addEventListener("mouseover", changeColor);
-})
+dimensions = document.querySelector("#dimensions");
+dimensions.addEventListener("click", handlePrompt)
 
 function changeColor(e) {
     this.style.backgroundColor = "black";
 }
 
+function createGrid(n) {
+    for (let i = 0; i < n; i++) {
+        row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < n; j++) {
+            rowItem = document.createElement("div");
+            rowItem.classList.add("row-item");
+            row.appendChild(rowItem)
+        }
+        container.appendChild(row)
+    }
+
+    rowItems = document.querySelectorAll(".row-item");
+    rowItems.forEach(item => {
+        item.addEventListener("mouseover", changeColor);
+    })
+}
+
+function handlePrompt() {
+    const size = prompt("Enter the number of boxes (vertically and horizontally):")
+    if ((!size) || (size > 66) || (!Number.isInteger(parseInt(size)))) return;
+    container.innerHTML = "";
+    createGrid(parseInt(size));
+}
