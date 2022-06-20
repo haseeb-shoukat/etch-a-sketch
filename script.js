0064ffbcc6c28e4b4deda5d11a0ceec78025f30b
hasemon <abdulhaseebshoukat24@outlook.con>
2022-06-20 11:08:01 +0500

    Write js code to create 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+container = document.querySelector(".container")
+
+for (let i = 0; i < 16; i++) {
+    row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+        rowItem = document.createElement("div");
+        rowItem.classList.add("row-item");
+        row.appendChild(rowItem)
+    }
+    container.appendChild(row)
+}
